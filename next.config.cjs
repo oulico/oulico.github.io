bf26c078ede8b7538d5a3af7f85ec0898fb74ba0
JH Lee <manemis@gmail.com>
2023-02-26 11:27:34 +0900

    FIX: Manage Css, assets, and page links to work properly

--- next.config.cjs
+++ next.config.cjs
@@ -1,97 +1,111 @@
-//const withImages = require("next-images");
 //const withPlugins = require("next-compose-plugins");
 //const optimizedImages = require("next-optimized-images");
-//const withMDX = require("@next/mdx")({
-//	extension: /\.mdx?$/,
-//});
 //
-//module.exports = withPlugins([
-//	optimizedImages,
-//	withMDX({
-//		webpack: (config, { isServer }) => {
-//			if (!isServer) {
-//				config.node = {
-//					fs: "empty",
-//				};
-//			}
-//			return config;
-//		},
-//		experimental: {
-//			target: "serverless",
-//		},
-//		pageExtensions: ["js", "jsx", "mdx"],
-//	}),
-//]);
+//const withCustomImageOptimizer =
+//  () =>
+//  (nextConfig = {}) => {
+//    const { images = {} } = nextConfig;
+//    const { deviceSizes = [], formats = [], loader } = images;
+//
+//    nextConfig.images = {
+//      ...images,
+//      disableStaticImages: false,
+//      loader: "custom",
+//      optimizeImages: true,
+//      mozjpeg: {
+//        quality: 80,
+//      },
+//      optipng: {
+//        optimizationLevel: 3,
+//      },
+//    };
+//
+//    nextConfig.webpack = (config, options) => {
+//      if (!options.isServer && loader === "custom") {
+//        config.module.rules.push({
+//          test: /\.(jpe?g|png)$/i,
+//          use: [
+//            {
+//              loader: "responsive-loader",
+//              options: {
+//                adapter: require("responsive-loader/sharp"),
+//                quality: 80,
+//                sizes: deviceSizes,
+//                formats,
+//                name: "[name]-[hash].[ext]",
+//                outputPath: "static/images/",
+//                publicPath: "/_next/static/images/",
+//                esModule: false,
+//                emitFile: false,
+//              },
+//            },
+//            {
+//              loader: "imagemin-loader",
+//              options: {
+//                cache: true,
+//                imageminOptions: {
+//                  plugins: [
+//                    ["mozjpeg", { quality: 80 }],
+//                    ["optipng", { optimizationLevel: 3 }],
+//                  ],
+//                },
+//              },
+//            },
+//          ],
+//        });
+//      }
+//
+//      return config;
+//    };
+//
+//    return nextConfig;
+//  };
+//
+//module.exports = withPlugins(
+//  [
+//    [
+//      optimizedImages,
+//      {
+//        optimizeImages: true,
+//      },
+//    ],
+//  ],
+//  withCustomImageOptimizer()
+//);
+
+const path = require("path");
+const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
+
 const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
+const MDX = require('@next/mdx');
 
-const withCustomImageOptimizer = () => (nextConfig = {}) => {
-  const { images = {} } = nextConfig;
-  const { deviceSizes = [], formats = [], loader } = images;
+// next.js configuration
+const nextConfig = {
+	webpack5: false,
+	webpack: (config, { isServer }) => {
+	    if (!isServer) {
+	      	config.node = {
+	        fs: 'empty'
+	     	}
+	    }
+	    config.resolve.alias.images = path.join(__dirname, "images");
+	    return config
+	},
+	pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+	target: 'serverless',
+	exportPathMap: function () {
+		return {
+			'/': { page: '/' },
+			'/about': { page: '/about' },
+		}
+	},
+	basePath: ghPages? '/oulico.github.io/' : '',
+	assetPrefix: ghPages ? '/oulico.github.io/' : '',
+}
 
-  nextConfig.images = {
-    ...images,
-    disableStaticImages: false,
-    loader: 'custom',
-    optimizeImages: true,
-    mozjpeg: {
-      quality: 80,
-    },
-    optipng: {
-      optimizationLevel: 3,
-    },
-  };
-
-  nextConfig.webpack = (config, options) => {
-    if (!options.isServer && loader === 'custom') {
-      config.module.rules.push({
-        test: /\.(jpe?g|png)$/i,
-        use: [
-          {
-            loader: 'responsive-loader',
-            options: {
-              adapter: require('responsive-loader/sharp'),
-              quality: 80,
-              sizes: deviceSizes,
-              formats,
-              name: '[name]-[hash].[ext]',
-              outputPath: 'static/images/',
-              publicPath: '/_next/static/images/',
-              esModule: false,
-              emitFile: false,
-            },
-          },
-          {
-            loader: 'imagemin-loader',
-            options: {
-              cache: true,
-              imageminOptions: {
-                plugins: [
-                  ['mozjpeg', { quality: 80 }],
-                  ['optipng', { optimizationLevel: 3 }],
-                ],
-              },
-            },
-          },
-        ],
-      });
-    }
-
-    return config;
-  };
-
-  return nextConfig;
-};
-
-module.exports = withPlugins(
-  [
-    [
-      optimizedImages,
-      {
-        optimizeImages: true,
-      },
-    ],
-  ],
-  withCustomImageOptimizer()
-);
+module.exports = withPlugins([
+	[MDX, {
+		extension: /\.mdx?$/,
+	}]
+], nextConfig)
 
